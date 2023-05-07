0cc0c5cb5e24b5b1ac5e4abf8d9f0beb93f50422
benjaminellmer <benjamin.ellmer@yahoo.com>
2023-05-07 18:04:01 +0200

    Finish step2 (without properties)

--- ex03/translation-neo4j.sql
+++ ex03/translation-neo4j.sql
@@ -1,71 +1,60 @@
-CREATE (n:Folk {id: 100, name: 'Alex'});
-CREATE (n:Folk {id: 20, name: 'Dad'});
-CREATE (n:Folk {id: 30, name: 'Mom'});
-CREATE (n:Folk {id: 10, name: 'Grandpa Bill'});
-CREATE (n:Folk {id: 98, name: 'Sister Amy'});
+CREATE (alex:Folk {id: 100, name: 'Alex'})
+CREATE (dad:Folk {id: 20, name: 'Dad'})
+CREATE (mom:Folk {id: 30, name: 'Mom'})
+CREATE (grandpa:Folk {id: 10, name: 'Grandpa Bill'})
+CREATE (amy:Folk {id: 98, name: 'Sister Amy'})
 
+CREATE (dad)-[:FATHER]->(alex)
+CREATE (mom)-[:MOTHER]->(alex)
 
-CREATE (n:Location {vertex_id: 1, alias: 'NAmerica', label: 'Location', name: 'North America', type: 'continent'});
-CREATE (n:Location {vertex_id: 2, alias: 'Europe', label: 'Location', name: 'Europe', type: 'continent'});
-CREATE (n:Location {vertex_id: 3, alias: 'USA', label: 'Location', name: 'United States', type: 'country'});
-CREATE (n:Location {vertex_id: 4, alias: 'UK', label: 'Location', name: 'United Kingdom', type: 'country'});
-CREATE (n:Location {vertex_id: 5, alias: 'England', label: 'Location', name: 'England', type: 'country'});
-CREATE (n:Location {vertex_id: 6, alias: 'Austria', label: 'Location', name: 'Österreich', type: 'country'});
-CREATE (n:Location {vertex_id: 7, alias: 'Idaho', label: 'Location', name: 'Idaho', type: 'state'});
-CREATE (n:Location {vertex_id: 8, alias: 'London', label: 'Location', name: 'London', type: 'city'});
-CREATE (n:Location {vertex_id: 9, alias: 'UpperAustria', label: 'Location', name: 'Oberösterreich', type: 'Bundesland'});
-CREATE (n:Location {vertex_id: 10, alias: 'Waldviertel', label: 'Location', name: 'Waldviertel', type: 'Viertel'});
-CREATE (n:Location {vertex_id: 11, alias: 'Grein', label: 'Location', name: 'Grein', type: 'city'});
+CREATE (dad)-[:FATHER]->(amy)
+CREATE (mom)-[:MOTHER]->(amy)
 
-CREATE (n:Person {vertex_id: 12, alias: 'Andrea', label: 'Person', name: 'Andrea', type: 'person'});
-CREATE (n:Person {vertex_id: 13, alias: 'Bert', label: 'Person', name: 'Bert', type: 'person'});
-CREATE (n:Person {vertex_id: 14, alias: 'Christian', label: 'Person', name: 'Christian', type: 'person'});
+CREATE (alex)-[:BROTHER]->(amy)
+CREATE (amy)-[:SISTER]->(alex)
 
+CREATE (mom)-[:MARRIED]->(dad)
+CREATE (dad)-[:MARRIED]->(mom)
 
-MATCH (tail:Location {vertex_id: 3}), (head:Location {vertex_id: 1})
-CREATE (tail)-[:WITHIN]->(head);
+CREATE (grandpa)-[:FATHER]->(dad);
 
-MATCH (tail:Location {vertex_id: 4}), (head:Location {vertex_id: 2})
-CREATE (tail)-[:WITHIN]->(head);
+CREATE (namerica:Location {alias: 'NAmerica', label: 'Location', name: 'North America', type: 'continent'})
+CREATE (europe:Location {alias: 'Europe', label: 'Location', name: 'Europe', type: 'continent'})
+CREATE (usa:Location {alias: 'USA', label: 'Location', name: 'United States', type: 'country'})
+CREATE (uk:Location {alias: 'UK', label: 'Location', name: 'United Kingdom', type: 'country'})
+CREATE (england:Location {alias: 'England', label: 'Location', name: 'England', type: 'country'})
+CREATE (austria:Location {alias: 'Austria', label: 'Location', name: 'Österreich', type: 'country'})
+CREATE (idaho:Location {alias: 'Idaho', label: 'Location', name: 'Idaho', type: 'state'})
+CREATE (london:Location {alias: 'London', label: 'Location', name: 'London', type: 'city'})
+CREATE (upperaustria:Location {alias: 'UpperAustria', label: 'Location', name: 'Oberösterreich', type: 'Bundesland'})
+CREATE (waldviertel:Location {alias: 'Waldviertel', label: 'Location', name: 'Waldviertel', type: 'Viertel'})
+CREATE (grein:Location {alias: 'Grein', label: 'Location', name: 'Grein', type: 'city'})
 
-MATCH (tail:Location {vertex_id: 5}), (head:Location {vertex_id: 4})
-CREATE (tail)-[:WITHIN]->(head);
+CREATE (usa)-[:WITHIN]->(namerica)
+CREATE (uk)-[:WITHIN]->(europe)
+CREATE (england)-[:WITHIN]->(uk)
+CREATE (austria)-[:WITHIN]->(europe)
+CREATE (idaho)-[:WITHIN]->(usa)
+CREATE (london)-[:WITHIN]->(england)
+CREATE (upperaustria)-[:WITHIN]->(austria)
+CREATE (waldviertel)-[:WITHIN]->(upperaustria)
+CREATE (grein)-[:WITHIN]->(waldviertel);
 
-MATCH (tail:Location {vertex_id: 6}), (head:Location {vertex_id: 2})
-CREATE (tail)-[:WITHIN]->(head);
+MATCH (idaho:Location {alias: 'Idaho'})
+MATCH (london:Location {alias: 'London'})
+MATCH (grein:Location {alias: 'Grein'})
 
-MATCH (tail:Location {vertex_id: 7}), (head:Location {vertex_id: 3})
-CREATE (tail)-[:WITHIN]->(head);
+CREATE (andrea:Person {alias: 'Andrea', label: 'Person', name: 'Andrea', type: 'person'})
+CREATE (bert:Person {alias: 'Bert', label: 'Person', name: 'Bert', type: 'person'})
+CREATE (christian:Person {alias: 'Christian', label: 'Person', name: 'Christian', type: 'person'})
 
-MATCH (tail:Location {vertex_id: 8}), (head:Location {vertex_id: 5})
-CREATE (tail)-[:WITHIN]->(head);
+CREATE (andrea)-[:BORN_IN]->(idaho)
+CREATE (andrea)-[:LIVES_IN]->(london)
 
-MATCH (tail:Location {vertex_id: 9}), (head:Location {vertex_id: 6})
-CREATE (tail)-[:WITHIN]->(head);
+CREATE (bert)-[:BORN_IN]->(grein)
+CREATE (bert)-[:LIVES_IN]->(london)
 
-MATCH (tail:Location {vertex_id: 10}), (head:Location {vertex_id: 9})
-CREATE (tail)-[:WITHIN]->(head);
-
-MATCH (tail:Location {vertex_id: 11}), (head:Location {vertex_id: 10})
-CREATE (tail)-[:WITHIN]->(head);
-
-MATCH (tail:Person {vertex_id: 12}), (head:Location {vertex_id: 7})
-CREATE (tail)-[:BORN_IN]->(head);
-
-MATCH (tail:Person {vertex_id: 12}), (head:Location {vertex_id: 8})
-CREATE (tail)-[:LIVES_IN]->(head);
-
-MATCH (tail:Person {vertex_id: 13}), (head:Location {vertex_id: 11})
-CREATE (tail)-[:BORN_IN]->(head);
-
-MATCH (tail:Person {vertex_id: 13}), (head:Location {vertex_id: 8})
-CREATE (tail)-[:LIVES_IN]->(head);
-
-MATCH (tail:Person {vertex_id: 14}), (head:Location {vertex_id: 8})
-CREATE (tail)-[:BORN_IN]->(head);
-
-MATCH (tail:Person {vertex_id: 12}), (head:Person {vertex_id: 13})
-CREATE (tail)-[:MARRIED]->(head);
-
-MATCH (tail:Person {vertex_id: 13}), (head:Person {vertex_id: 12})
-CREATE (tail)-[:MARRIED]->(head);
+CREATE (christian)-[:BORN_IN]->(london)
+	
+CREATE (andrea)-[:MARRIED]->(bert)
+CREATE (bert)-[:MARRIED]->(andrea);
